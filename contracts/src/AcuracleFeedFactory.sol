bd3b8f8bc85fe1512617f255e2ec480c8fadc2b7
Emmanuel <opensoftweb@gmail.com>
2024-03-08 21:50:59 +0100

    feat: implement factory for deploying new price feeds

---
+++ contracts/src/AcuracleFeedFactory.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.23;
+
+import {IAcuracleFeedFactory} from "./interfaces/IAcuracleFeedFactory.sol";
+import {Owned} from "./Owned.sol";
+import {Acuracle} from "./Acuracle.sol";
+
+contract AcuracleFeedFactory is IAcuracleFeedFactory, Owned {
+    struct AccountFeed {
+        uint256 nonce;
+        address[] feeds;
+    }
+
+    /**
+     * @dev keep track of account feed and nonce
+     */
+    mapping(address => AccountFeed) private _accountFeeds;
+
+    constructor() Owned(msg.sender) {}
+
+    /// @inheritdoc IAcuracleFeedFactory
+    function createFeed(uint8 decimals, string memory name) external override {
+        uint256 nonce = _accountFeeds[msg.sender].nonce;
+        address feed = address(
+            new Acuracle{salt: keccak256(abi.encode(msg.sender, nonce))}(
+                msg.sender,
+                decimals,
+                name
+            )
+        );
+
+        _accountFeeds[msg.sender].nonce += 1;
+        _accountFeeds[msg.sender].feeds.push(feed);
+
+        emit CreateFeed(feed, msg.sender);
+    }
+
+    /// @inheritdoc IAcuracleFeedFactory
+    function getFeeds(
+        address account
+    ) external view override returns (address[] memory) {
+        return _accountFeeds[account].feeds;
+    }
+}
--- contracts/src/interfaces/IAcuracleFeedFactory.sol
+++ contracts/src/interfaces/IAcuracleFeedFactory.sol
@@ -3,5 +3,22 @@
 pragma solidity ^0.8.23;
 
 interface IAcuracleFeedFactory {
-    
+    /**
+     * @notice Emitted whenever a new feed is deployed
+     */
+    event CreateFeed(address indexed feed, address indexed creator);
+
+    /**
+     * @dev Allow anyone to easily deploy a new price feed
+     *
+     * @param decimals the oracle feed decimal
+     * @param name the oracle tick symbol or name
+     */
+    function createFeed(uint8 decimals, string memory name) external;
+
+    /**
+     * @dev Fetch all price feeds deployed by an account
+     * @param account the account to filter for
+     */
+    function getFeeds(address account) external view returns (address[] memory);
 }
